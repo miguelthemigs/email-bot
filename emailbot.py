b2a5b4e88800f8fb0762deda5449ee8f533d9da0
Miguel Werneck <93150152+miguelthemigs@users.noreply.github.com>
2022-02-26 11:20:43 -0300

    Add files via upload

---
+++ emailbot.py
@@ -0,0 +1,41 @@
+
+import os
+import smtplib
+import time
+from email.message import EmailMessage
+
+email_address = os.environ.get('DB_USER')
+email_password = os.environ.get('DB_PASS')
+
+msg = EmailMessage()
+msg['Subject'] = 'BOM DIA'
+msg['From'] = email_address
+msg['To'] = 'miguelthemigs@gmail.com'
+msg.set_content('bom dia linda')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_address, email_password)
+
+    smtp.send_message(msg)
+
+    message_loop = 0
+
+    for message_loop in range(1, 100):
+        message_loop = message_loop + 1
+        smtp.send_message(msg)
+        time.sleep(1)
+
+        if message_loop == 100:
+            break
+
+
+
+
+
+
+
+
+
+
+
